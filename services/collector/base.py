0a00bf952853bc8bc880f8d1ce07e210617796dc
baltikaa9 <sergey4016@mail.ru>
2023-07-25 20:57:52 +0300

    abstract

--- services/collector/base.py
+++ services/collector/base.py
@@ -1,12 +1,16 @@
+from abc import abstractmethod, ABC
+
 from schemas.base import BaseSchema
 
 
-class BaseCollector:
+class BaseCollector(ABC):
     def __init__(self):
         self.storage: list[BaseSchema] = []
 
+    @abstractmethod
     async def fetch(self) -> list:
-        ...
+        raise NotImplementedError
 
+    @abstractmethod
     async def save_to_db(self) -> None:
-        ...
+        raise NotImplementedError
